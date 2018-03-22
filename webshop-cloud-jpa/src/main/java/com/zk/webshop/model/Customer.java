2e1339ce1701540d4617c7aa4b3340d3358a46a8
Zakhar <kvzakhar@yandex.ru>
2018-03-22 22:35:42 +0300

    commit

---
+++ webshop-cloud-jpa/src/main/java/com/zk/webshop/model/Customer.java
@@ -0,0 +1,10 @@
+package com.zk.webshop.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "WEBSHOP_CUSTOMER")
+public class Customer {
+
+}
--- webshop-cloud-jpa/src/main/java/com/zk/webshop/model/Product.java
+++ webshop-cloud-jpa/src/main/java/com/zk/webshop/model/Product.java
@@ -1,10 +1,24 @@
 package com.zk.webshop.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "WEBSHOP_PRODUCT")
 public class Product {
+	
+	@Id
+	@Column(name="PRODUCT_ID", length = 10)
+	private String productId;
+	
+	private String name;
+	
+	@Column(name = "SHORT_DESCRIPTION")
+	private String shortDescription;
+
+	@Column(name = "LONG_DESCRIPTION")
+	private String longDescription;
 
 }
